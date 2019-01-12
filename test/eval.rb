cdaefa0c8f5734e34f0b8651c429b161cbd6f777
Rich Pelavin <rich@reactor8.com>
2019-01-12 22:15:06 +0000

    updated eval.rb so it can handle statements that are multiline

--- test/eval.rb
+++ test/eval.rb
@@ -1,11 +1,41 @@
 #!/usr/bin/env ruby
-require 'z3'
 file = ARGV[0]
-File.open(file).each_line do |line|
-  begin 
-    STDOUT << ::Z3::LowLevel.eval_smtlib2_string(line)
-  rescue => e
-    STDOUT << "Error for:\n  #{line}"
-    fail e
+
+module DTK
+  require 'z3'
+  class Z3
+    def self.read_and_return_statements(file)
+      ret = []
+      open_parens_diff = 0
+      statement        = ''
+      File.open(file).each_line do |line|
+        statement << line
+        diffs = line.scan('(').size - line.scan(')').size
+        open_parens_diff += diffs
+        if open_parens_diff == 0
+          ret << statement
+          statement = ''
+
+        elsif open_parens_diff > 0
+          # next line wil be included
+        else
+          fail "Unexected that open_parens_diff (#{open_parens_diff}) is < 0"
+        end
+      end
+      ret
+    end
+
+    def self.process(statement)
+      begin 
+        ::Z3::LowLevel.eval_smtlib2_string(statement)
+      rescue => e
+        STDOUT << "Error for:\n  #{statement}"
+        fail e
+      end
+    end
   end
+end
+
+DTK::Z3.read_and_return_statements(file).each do |statement|
+  STDOUT << DTK::Z3.process(statement)
 end
