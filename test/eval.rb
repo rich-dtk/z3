58e89af4ed92e83b6e788ef9d7aecc85cbe52bd1
Rich Pelavin <rich@reactor8.com>
2019-01-13 20:17:28 +0000

    updated eval.rb

--- test/eval.rb
+++ test/eval.rb
@@ -6,21 +6,30 @@ module DTK
   class Z3
     def self.read_and_return_statements(file)
       ret = []
-      open_parens_diff = 0
-      statement        = ''
+      open_parens = 0
+      statement  = ''
       File.open(file).each_line do |line|
-        statement << line
-        diffs = line.scan('(').size - line.scan(')').size
-        open_parens_diff += diffs
-        if open_parens_diff == 0
-          ret << statement
-          statement = ''
+        unless line =~ /^;/
+          if open_parens > 0 and not (line =~ /^ /)
+            fail "statement:\n  #{statement}\n is not closed but next line (#{line}) does not start with an indent"
+          end
 
-        elsif open_parens_diff > 0
-          # next line wil be included
-        else
-          fail "Unexected that open_parens_diff (#{open_parens_diff}) is < 0"
+          statement << line
+
+          paren_diffs = line.scan('(').size - line.scan(')').size
+          open_parens += paren_diffs
+          if open_parens == 0
+            ret << statement
+            statement = ''
+          elsif open_parens_diff > 0
+            # nothing else needs to be done; statement << line has been done in above line
+          else
+            fail "Unexected that open_parens_diff (#{open_parens_diff}) is < 0"
+          end
         end
+      end
+      if open_parens > 0
+        fail "Last statement\n #{statement}\n was not closed and end of file has been reached"
       end
       ret
     end
